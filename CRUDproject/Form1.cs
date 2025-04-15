afb1064b927e945a5c06a91a09ed2ce6642be7b1
Notsume <adahorapirola@gmail.com>
2025-04-15 11:17:44 -0300

    Preparação Formulário - Radio Buttons e Checkbox

--- CRUDproject/Form1.cs
+++ CRUDproject/Form1.cs
@@ -56,5 +56,15 @@ namespace CRUDproject
         {
 
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
